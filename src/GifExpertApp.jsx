91cbaa262884e17bbae095237735af874143d4b6
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-10 19:13:54 -0300

    Agregar nueva categoria

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
-    return (
-        <>
-            <h1>GifExpertApp</h1>
-        </>
-    );
-}
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const onAddCategory = () => {
+    setCategories(['Naruto', ...categories]);
+    // setCategories(cat => [...cat, 'Naruto'])
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
 
 export default GifExpertApp;
