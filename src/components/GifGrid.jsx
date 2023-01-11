07b9edd04b8fe81b2b12a282c83ab0f1d1af61ad
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-11 17:03:55 -0300

    useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
 
-const GifGrid = ({category}) => {
-    return (
-        <>
-            <h3>{category}</h3>
-        </>
-    );
-}
+const GifGrid = ({ category }) => {
+  useEffect(() => {
+    getGifs(category);
+  }, []);
+
+  return (
+    <>
+      <h3>{category}</h3>
+    </>
+  );
+};
 
 export default GifGrid;
