940cd677fd79ff26a2999c59fa48d3c830a879d4
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-11 03:15:48 -0300

    Nuevo Componente gifGrid

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import AddCategory from './components/AddCategory';
+import GifGrid from './components/GifGrid';
 
 const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
   const onAddCategory = (newCategory) => {
     if (categories.includes(newCategory)) return;
-   
+
     setCategories([newCategory, ...categories]);
     // setCategories(cat => [...cat, 'Naruto'])
   };
@@ -15,11 +16,9 @@ const GifExpertApp = () => {
     <>
       <h1>GifExpertApp</h1>
       <AddCategory onNewCategory={(value) => onAddCategory(value)} />
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
