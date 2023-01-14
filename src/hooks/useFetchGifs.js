beb62e6da5222a6718006934a4d8897c7a569c04
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-13 22:33:23 -0300

    pruebas sobre el custom hook

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getGifs } from '../helpers/getGifs';
+
 export const useFetchGifs = (category) => {
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
