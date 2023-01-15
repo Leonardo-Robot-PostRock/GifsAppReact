74fa342d4b59c5090fedbd51516c43dd410581b0
Leonardo Puebla <leonardo38361@gmail.com>
2023-01-15 02:07:51 -0300

    test en gifexpertApp

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,8 +1,30 @@
-import { render } from "@testing-library/react";
-import GifExpertApp from "../src/GifExpertApp";
+import { fireEvent, render, screen } from '@testing-library/react';
+import GifExpertApp from '../src/GifExpertApp';
 
-describe('Pruebas en <GifExpertApp />', () => { 
-    test('', () => {
-        render(<GifExpertApp />)
-    });
- })
+describe('Pruebas en <GifExpertApp />', () => {
+  test('debe escribir en el input', () => {
+    // Sujeto de pruebas
+    render(<GifExpertApp />);
+
+    // Disparamos el eventos
+    const input = screen.getByRole('textbox');
+    fireEvent.input(input, { target: { value: 'Saitama' } });
+
+    // Hacemos la aserción de lo que esperamos que suceda
+    expect(input.value).toBe('Saitama');
+
+    // screen.debug();
+  });
+
+  test('no debe añadir una categoría existente', () => {
+    render(<GifExpertApp />);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input, { target: { value: 'One Piece' } });
+    fireEvent.submit(form);
+
+    expect(screen.getAllByText('One Piece').length).toBe(1);
+  });
+});
